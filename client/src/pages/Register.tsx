dbe997bf7945768436517fe9e4afd47254baa111
SarthakMehta03 <sarthakmehta512@gmail.com>
2026-02-28 02:19:11 +0530

    Created Some Folders

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const { register } = useAuth();
+  const navigate = useNavigate();
+
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    await register(form.name, form.email, form.password);
+    navigate("/login");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Name"
+        onChange={(e) => setForm({ ...form, name: e.target.value })}
+      />
+      <input
+        placeholder="Email"
+        onChange={(e) => setForm({ ...form, email: e.target.value })}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        onChange={(e) => setForm({ ...form, password: e.target.value })}
+      />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
